a2b27ce80e9494cc67a7181a030efe24fa201d0f
cooper-bocko-uga <105941591+cooperbocko@users.noreply.github.com>
2025-03-17 21:43:04 -0400

    edits
    ...

--- javamulticast/src/main/java/com/mycompany/app/coordinator/Coordinator.java
+++ javamulticast/src/main/java/com/mycompany/app/coordinator/Coordinator.java
@@ -1,17 +1,13 @@
 package com.mycompany.app.coordinator;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.Iterator;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ExecutorService;
@@ -71,9 +67,8 @@ public class Coordinator {
                     System.out.println("Client connected: " + clientChannel.getRemoteAddress());
                 } else if (key.isReadable()) {
                     //handle client requests
-                    SocketChannel clientChannel = (SocketChannel) key.channel();
                     //threadPool.submit(new DoSomething(clientChannel));
-                    new HandleRequest(clientChannel, key).run();
+                    new HandleRequest(key).run();
                 }
             }
         }
@@ -85,8 +80,8 @@ public class Coordinator {
         private final SocketChannel participant;
         private final SelectionKey key;
 
-        public HandleRequest(SocketChannel participant, SelectionKey key) {
-            this.participant = participant;
+        public HandleRequest(SelectionKey key) {
+            this.participant = (SocketChannel) key.channel();
             this.key = key;
         }
 
@@ -95,14 +90,13 @@ public class Coordinator {
             try {
                 ByteBuffer buf = ByteBuffer.allocate(MAX_MESSAGE_LENGTH);
                 int bytesRead = participant.read(buf);
-
                 if (bytesRead == -1) {
                     System.out.println("Read Error");
                     participant.close();
                     key.cancel();
-                    return;
+                    System.out.println("Participant Disconnect"); 
+                    return; //cannot read from channel -> ERROR
                 }
-
                 if (bytesRead == 0) {
                     return; //sometimes there is nothing to read
                 }
@@ -111,17 +105,17 @@ public class Coordinator {
                 buf.flip();
                 String request = new String(buf.array(), 0, buf.limit());
                 System.out.println("participant " + participant.getRemoteAddress() + ": " + request);
-
                 String[] parsedRequest = request.split(" ");
+                
                 switch(parsedRequest[0].toLowerCase()) {
                     case "register": {
                         //format -> register id ip port (assume it is correct format)
                         int id = Integer.parseInt(parsedRequest[1]);
                         String ip = parsedRequest[2];
                         int port = Integer.parseInt(parsedRequest[3]);
-
                         if (connections.containsKey(id)) {
                             //TODO: Send error
+                            sendMessage(participant, "Already Registered!");
                             break;
                         }
 
@@ -134,22 +128,25 @@ public class Coordinator {
                         connections.put(id, conn);
 
                         //TODO: Send ack
+                        sendMessage(participant, "Registered!");
                         break;
                     }
                     case "deregister": {
                         //format -> deregister id
-                        //remove from map and close channel/key
                         int id = Integer.parseInt(parsedRequest[1]);
                         if (!connections.containsKey(id)) {
                             //TODO: Send error
+                            sendMessage(participant, "Not Registered!");
                             break;
                         }
+                        
                         connections.remove(id);
                         //TODO: is connection closed?
                         //participant.close();
                         //key.cancel();
 
                         //TODO: Send ack
+                        sendMessage(participant, "Deregistered!");
                         break;
                     }
                     case "disconnect": {
@@ -157,11 +154,14 @@ public class Coordinator {
                         int id = Integer.parseInt(parsedRequest[1]);
                         if (!connections.containsKey(id) || !connections.get(id).isOnline) {
                             //TODO: Send error
+                            sendMessage(participant, "Not registered or already disconnected!");
                             break;
                         }
 
                         connections.get(id).isOnline = false;
+
                         //TODO: Send ack
+                        sendMessage(participant, "Disconnected!");
                         break;
                     }
                     case "reconnect": {
@@ -169,73 +169,75 @@ public class Coordinator {
                         int id = Integer.parseInt(parsedRequest[1]);
                         if (!connections.containsKey(id) || connections.get(id).isOnline) {
                             //TODO: Send error
+                            sendMessage(participant, "Not registered or already online!");
                             break;
                         }
 
                         connections.get(id).isOnline = true;
                         //TODO: send messages that were not sent
+
                         //TODO: Send ack
+                        sendMessage(participant, "Reconnected!");
                         break;
                     }
                     case "multicast": {
-                        //format multicast message
+                        //format multicast id message
+                        int id = Integer.parseInt(parsedRequest[1]);
+                        if (!connections.contains(id) || !connections.get(id).isOnline) {
+                            //TODO: send error
+                            sendMessage(participant, "Not registered or not online!");
+                            break;
+                        }
+
+                        Message newMessage = new Message();
+                        newMessage.idsToSend = connections.keys();
+                        newMessage.message = parsedRequest[2];
+                        newMessage.time = LocalDateTime.now();
+                        messageQueue.add(newMessage);
+                        messageList.add(newMessage);
+
+                        //TODO: send ack
+                        sendMessage(participant, "Message Sent!");
+                        break;
+                    }
+                    default: {
+                        sendMessage(participant, "Unknown Request: " + parsedRequest[0]);
+                        break;
                     }
                 }
-                
-                String response = "World!";
-                buf.clear();
-                buf.put(response.getBytes());
-                buf.flip();
-                participant.write(buf);
-
             } catch (IOException e) {
                 System.out.println("Participant Disconnect"); 
                 key.cancel();
             }
         }
-
     }
-    /*
-     * static class DoSomething implements Runnable {
-        private final SocketChannel clientChannel;
-        private final SelectionKey key;
 
-        public DoSomething(SocketChannel clientChannel, SelectionKey key) {
-            this.clientChannel = clientChannel;
-            this.key = key;
+    //TODO: Maybe change it from thorwing an error to catching the error and returning -1
+    //TODO: Also maybe migrate to a util file since 
+    public static void sendMessage (SocketChannel channel, String message) throws IOException {
+        ByteBuffer buf = ByteBuffer.allocate(MAX_MESSAGE_LENGTH);
+        buf.put(message.getBytes());
+        buf.flip();
+        channel.write(buf);
+    }
+
+
+    //call by value cant return message and possible errors at the same time without a created type
+    /* 
+    public static int recieveMessage (SocketChannel channel, String message) throws IOException{
+        ByteBuffer buf = ByteBuffer.allocate(MAX_MESSAGE_LENGTH);
+        int bytesRead = channel.read(buf);
+
+        if (bytesRead == -1) {
+            return -1; //Read Error
+        }
+        if (bytesRead == 0) {
+            return 0; //sometimes there is nothing to read
         }
 
-        @Override
-        public void run() {
-            try {
-                ByteBuffer buf = ByteBuffer.allocate(256);
-                int bytesRead = clientChannel.read(buf);
-
-                if (bytesRead == -1) {
-                    System.out.println("Client disconnected: " + clientChannel.getRemoteAddress());
-                    clientChannel.close();
-                    return;
-                }
-
-                if (bytesRead == 0) {
-                    return;
-                }
-
-                buf.flip();
-                String message = new String(buf.array(), 0, buf.limit());
-                System.out.println("Clinet: " + message);
-
-                String response = "World!";
-                buf.clear();
-                buf.put(response.getBytes());
-                buf.flip();
-                clientChannel.write(buf);
-            } catch (IOException e) {
-                System.out.println("Client Disconnect");
-                key.cancel();
-            }
-        }
+        buf.flip();
+        message = new String(buf.array(), 0, buf.limit());
+        return 1;
     }
-     */
-    
+    */
 }

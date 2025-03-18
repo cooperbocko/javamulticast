927939f84ef7a66efc23b302c083ea1fa3a716c4
nai50915 <nai50915@uga.edu>
2025-03-18 08:06:33 -0400

    Parse participant
    Question: what if participant config file isn't where participant is?

--- javamulticast/src/main/java/com/mycompany/app/participant/Participant.java
+++ javamulticast/src/main/java/com/mycompany/app/participant/Participant.java
@@ -1,5 +1,7 @@
 package com.mycompany.app.participant;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -7,12 +9,37 @@ import java.nio.channels.SocketChannel;
 
 public class Participant {
 
+    private int participantId;
+    private String messageLogFile;
+    private String coordinatorIp;
+    private int coordinatorPort; 
+
+    public Participant(String configFile) throws IOException {
+        parseConfigFile(configFile);
+    }
+
+    private void parseConfigFile(String configFile) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(configFile))) {
+            this.participantId = Integer.parseInt(reader.readLine().trim());
+            this.messageLogFile = reader.readLine().trim();
+            String[] coordinatorInfo = reader.readLine().trim().split(" ");
+            this.coordinatorIp = coordinatorInfo[0];
+            this.coordinatorPort = Integer.parseInt(coordinatorInfo[1]);
+        }
+        System.out.println("Participant ID: " + participantId);
+        System.out.println("Message Log File: " + messageLogFile);
+        System.out.println("Coordinator IP: " + coordinatorIp);
+        System.out.println("Coordinator Port: " + coordinatorPort);
+    }
+
     /*
      * Time of last message
      * One thread to send a message
      * One thread to recieve data and write to conf
      */
     public static void main(String args[]) throws IOException {
+        Participant p = new Participant(args[0]); 
+        
         SocketChannel channel = null;
 
         channel = SocketChannel.open();
